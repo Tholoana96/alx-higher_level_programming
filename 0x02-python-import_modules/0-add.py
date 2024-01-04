643909529a2875d5fa2df270caf25ff370a7e891
Tholoana <tholoana.manyane@gmail.com>
2024-01-04 19:43:27 +0200

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-print_list_integer = __import__('0-print_list_integer').print_list_integer
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
